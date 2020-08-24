053d8f3207234fc1ee3bac9369aa6507bad5bfaa
Sillas Reis <sillas.dev@gmail.com>
2020-08-24 15:31:51 -0300

    Atualizando tempo em manager_data.txt

--- video_run.py
+++ video_run.py
@@ -1,4 +1,5 @@
 import pyautogui as pygui
+import clipboard
 import time
 import sys
 import os
@@ -23,8 +24,32 @@ def open_video(last_time):
     time.sleep(5)
 
     # Sequência de comandos para avançar para tempo específico dentro do VLC Media Player
-    pygui.hotkey('ctrl', 't')
-    time.sleep(0.2)
+    pygui.hotkey('ctrl', 'g')
     pygui.write(last_time[1])
-    pygui.press(['tab', 'tab', 'enter'])
+    pygui.press('enter')
 
+
+# Obter tempo atual do vídeo
+def update_time():
+    # Obtendo tempo
+    pygui.hotkey('ctrl', 'g')
+    pygui.hotkey('ctrl', 'a')
+    pygui.hotkey('ctrl', 'c')
+    pygui.press('esc')
+
+    # Atualizando manager_data
+    manager_data = open('manager_data.txt','r', encoding='utf-8')
+    last_time = manager_data.read().split('\n')
+    manager_data.close()
+
+    manager_data = open('manager_data.txt','w', encoding='utf-8')
+    manager_data.write(f'{last_time[0]}\n{clipboard.paste()}')
+    manager_data.close()
+
+    
+last_time = last_time()
+open_video(last_time)
+
+while True:
+    time.sleep(10)
+    update_time()
