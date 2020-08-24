ddc03288a6d986eee71a932185c972b7945bf294
Sillas Reis <sillas.dev@gmail.com>
2020-08-24 12:59:53 -0300

    Abrindo vídeo em tempo específico

--- video_run.py
+++ video_run.py
@@ -11,3 +11,20 @@ def last_time():
     manager_data.close()
 
     return last_time
+
+
+# Abrir vídeo em tempo específico
+def open_video(last_time):
+    # Comando para abrir vídeo varia de acordo com o sistema
+    if sys.platform == 'win32':
+        os.startfile(last_time[0])
+
+    # Espera para dar tempo do vídeo abrir
+    time.sleep(5)
+
+    # Sequência de comandos para avançar para tempo específico dentro do VLC Media Player
+    pygui.hotkey('ctrl', 't')
+    time.sleep(0.2)
+    pygui.write(last_time[1])
+    pygui.press(['tab', 'tab', 'enter'])
+
