e0de51e5d25127ee80761eda53d187cba60a2208
Sillas Reis <sillas.dev@gmail.com>
2020-08-31 13:57:39 -0300

    Criando novas sessões

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@ import generator
 import video_run
 import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
 from os import listdir
 from os.path import isfile, join
 from glob import glob
@@ -30,6 +31,45 @@ class ScrollableFrame(Frame):
         scrollbar.pack(side="right", fill="y")
 
 
+class NewSectionWindow(Toplevel):
+    def __init__(self, transient):
+        super().__init__()
+
+        self.transient = transient
+        self.focus_force()
+        self.grab_set()
+
+        #self.sect_name = StringVar()
+        self.sect_path = None
+
+        self.resizable(False, False)
+
+        self.initInputWindow()
+    
+    def initInputWindow(self):
+        self.title('Nova sessão')
+
+        Label(self, text='Nome da sessão:').grid(row=1)
+        self.sect_name_entry = Entry(self)
+        self.sect_name_entry.grid(row=1, column=1, columnspan=2)
+        Label(self, text='').grid(row=3, columnspan=3)
+        Button(self, text='Cancelar', command=self.cancel_btn).grid(row=4, column=1, sticky='e')
+        Button(self, text='Continuar', command=self.continue_btn).grid(row=4, column=2)
+
+    def cancel_btn(self):
+        self.destroy()
+
+    def continue_btn(self):
+        sect_name = self.sect_name_entry.get()
+        
+        if len(sect_name) != 0:
+            generator.generate(sect_name)
+            self.transient.clear_sections_frames()
+            self.transient.initSections()
+            self.destroy()
+        else:
+            messagebox.showwarning(title='Nome inválido', message='Você precisa escolher um nome para a sessão!')
+
 class Menu(Frame):
     def __init__(self):
         super().__init__()
@@ -54,15 +94,15 @@ class Menu(Frame):
         self.pack(fill=BOTH, expand=True)
 
         # Botões para adicionar e remover seções
-        new_section_bt = Button(self, text='+ Nova seção', bg='lightgreen', height=1)
+        new_section_bt = Button(self, text='+ Nova seção', bg='lightgreen', height=1, command=self.add_section)
         new_section_bt.pack(side=LEFT, fill=BOTH, expand=True)
         remove_section_bt = Button(self, text='- Remover seção', bg='red', height=1)
         remove_section_bt.pack(side=RIGHT, fill=BOTH, expand=True)
 
-
     def initSections(self):
         sections_path = glob('./sections/*.fmng')
         self.sections_data = list()
+        self.sections_frames = list()
 
         # Carregar dados das seções
         for section in sections_path:
@@ -83,6 +123,16 @@ class Menu(Frame):
             Label(sect_frame, text=f'', bg='purple').grid(row=3, column=1, columnspan=2)
             Button(sect_frame, text='Continuar\nassistindo', bg='grey').grid(row=0, column=0, rowspan=3)
             sect_frame.pack(fill=BOTH)
+            self.sections_frames.append(sect_frame)
+
+    def clear_sections_frames(self):
+        for frame in self.sections_frames:
+            frame.destroy()
+        self.sections_frames = list()
+        self.sections_data = list()
+
+    def add_section(self):
+        NewSectionWindow(self)
 
 
 
